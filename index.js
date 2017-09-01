16b0a6ef1578678b4c8cac7795bc811ca47bdc23
Juho Heikkilä <juho.heikkila@koodifarmi.fi>
2017-09-01 09:42:00 +0300

    -

--- index.js
+++ index.js
@@ -6,10 +6,27 @@ exports.authorize = function(hook_name, context, cb) {
     return cb([true]);
   }
   if (context.req.session.passport) {
-      var hasRights = httpGet(req.session.passport.sub, context.req.url);
-      console.debug("ep_oidc_authorize: Authorize success");
-      //return cb([true]);
-      return cb([hasRights]);
+      var authorizeUrl = process.env.ETHERPAD_AUTH_URL;
+      var xmlHttp = new XMLHttpRequest();
+      var data = new FormData();
+      data.append('sub', context.req.session.passport.sub);
+      data.append('url', context.req.url);
+
+      xmlHttp.open("GET", authorizeUrl, false); // false for synchronous request
+      xmlHttp.send(data);
+      console.debug("ep_oidc_authorize: responseText: " + xmlHttp.responseText);
+
+      var resp = JSON.parse(xmlHttp.responseText);
+      if (resp.hasRights == true)
+      {
+          console.debug("ep_oidc_authorize: Authorize success");
+          return cb([true]);
+      }
+      else
+      {
+          console.debug("ep_oidc_authorize: Unauthorized");
+          return cb([false]);
+      }
   } 
   else {
     console.debug('ep_oidc_authorize: auth not completed');
@@ -17,24 +34,3 @@ exports.authorize = function(hook_name, context, cb) {
   }
 };
 
-
-authorizeMe = function httpGet(sub, url) {
-    var authorizeUrl = process.env.ETHERPAD_AUTH_URL;
-    var xmlHttp = new XMLHttpRequest();
-    var data = new FormData();
-    data.append('sub', sub);
-    if(url == null)
-        data.append('url', "gotourl");
-    else
-        data.append('url', url);
-
-    xmlHttp.open("GET", authorizeUrl, false); // false for synchronous request
-    xmlHttp.send(data);
-    console.debug("ep_oidc_authorize: httpGet: responseText: " + xmlHttp.responseText);
-
-    var resp = JSON.parse(xmlHttp.responseText);
-    if (resp.hasRights == true)
-        return true;
-    else
-        return false;
-}
