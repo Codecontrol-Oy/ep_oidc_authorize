0de2400748d54f399124452ecb7940966c710f4a
Juho Heikkilä <juho.heikkila@koodifarmi.fi>
2017-09-01 11:17:40 +0300

    add try-catch, logging to console.log.

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 exports.authorize = function(hook_name, context, cb) {
-  console.debug("ep_oidc_authorize: Authorize in action");
+  console.log("ep_oidc_authorize: Authorize in action");
   if(context.resource == "/auth/callback")
   {
-      console.debug("ep_oidc_authorize: Callback passed");
+      console.log("ep_oidc_authorize: Callback passed");
     return cb([true]);
   }
   if (context.req.session.passport) {
-      var authorizeUrl = process.env.ETHERPAD_AUTH_URL;
-      var xmlHttp = new XMLHttpRequest();
-      var data = new FormData();
-      data.append('sub', context.req.session.passport.sub);
-      data.append('url', context.req.url);
 
-      xmlHttp.open("GET", authorizeUrl, false); // false for synchronous request
-      xmlHttp.send(data);
-      console.debug("ep_oidc_authorize: responseText: " + xmlHttp.responseText);
+      try {
+          var authorizeUrl = process.env.ETHERPAD_AUTH_URL;
+          var xmlHttp = new XMLHttpRequest();
+          var data = new FormData();
+          data.append('sub', context.req.session.passport.sub);
+          data.append('url', context.req.url);
 
-      var resp = JSON.parse(xmlHttp.responseText);
-      if (resp.hasRights == true)
-      {
-          console.debug("ep_oidc_authorize: Authorize success");
-          return cb([true]);
+          xmlHttp.open("GET", authorizeUrl, false); // false for synchronous request
+          xmlHttp.send(data);
+          console.debug("ep_oidc_authorize: responseText: " + xmlHttp.responseText);
+
+          var resp = JSON.parse(xmlHttp.responseText);
+          if (resp.hasRights == true) {
+              console.debug("ep_oidc_authorize: Authorize success");
+              return cb([true]);
+          }
+          else {
+              console.debug("ep_oidc_authorize: Unauthorized");
+              return cb([false]);
+          }
+      } catch (e) {
+          console.log("ep_oidc_authorize ERROR: " + e.message);
+          return cb([true]); //return true for debugging
+
       }
-      else
-      {
-          console.debug("ep_oidc_authorize: Unauthorized");
-          return cb([false]);
-      }
+      
   } 
   else {
-    console.debug('ep_oidc_authorize: auth not completed');
+    console.log('ep_oidc_authorize: auth not completed');
     return cb([false]);
   }
 };
